d677439e094cf4a703f37802123570c0863cc9f8
vic2488 <poncev625@gmail.com>
2022-10-10 19:13:01 -0500

    Agregar archivos de proyecto.

---
+++ PryLogin/Controllers/AccesoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PryLogin.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
